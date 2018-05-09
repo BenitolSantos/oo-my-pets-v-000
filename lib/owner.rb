cd031054675f7056c0103c0d37d58108c9c202b1
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-05-09 20:19:23 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,8 +2,8 @@ require "pry"
 class Owner
   @@all = []
 
-  attr_reader :species, :pets
-  attr_accessor :name
+  attr_reader :species
+  attr_accessor :name, :pets
 
   def initialize(species)
     @species = species
